a1aba5bb065b58a46ac1359e29347cf103546b5c
Philipp Gallaschik <p,gallaschik@gmail.com>
2023-04-14 13:53:09 +0200

    changed the run.py (app = create_app() is now not in the if-statement

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app.app import create_app
 
-
+# took this line from the if-statement below (before app.run())
+app = create_app()
 
 if __name__ == '__main__':
-  app = create_app()
   app.run()
